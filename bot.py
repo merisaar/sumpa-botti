6317c1cc81d5f241912f42002a01e349fa3c652e
Wilhelm Törnroos <175621+Vilkku@users.noreply.github.com>
2025-06-11 13:12:16 +0300

    Restore logging and fix the syntax instead

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 def build_user_lookup():
     try:
         response = client.users_list(limit=999)
+        logger.info(f"users_list response_metadata: {response['response_metadata']}")
         users = response["members"]
         lookup = {user["name"]: user["id"] for user in users if not user.get("deleted", False)}
         return lookup
